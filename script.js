c3d4acef9ebc3086dd6f831492f0e7a76057d32d
samueldossantossilva <samueldossantossilva67@gmail.com>
2026-09-22 08:38:26 -0300

    Calculadora

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function subtrair() {
 
     document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
 }
+
+function multiplicar() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 * numero2;
+
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
+
+function dividir() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 / numero2;
+
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
